3d764cf3a2469b90b66ba3c7a66e4df76f936011
wxingfu <wxffxw.664587@gmail.com>
2023-02-11 21:55:41 +0800

    20230211

--- src/main/java/com/wxf/MyTest.java
+++ src/main/java/com/wxf/MyTest.java
@@ -1,54 +1,17 @@
 package com.wxf;
 
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import java.util.Scanner;
 
 public class MyTest {
 
     public static void main(String[] args) {
-        /*int a = 0;
-        System.out.println(a);*/
+        Scanner in = new Scanner(System.in);
 
-        /*byte b = 12;
-        int b1 = b + 1;
-        System.out.println(b1);*/
+        String str = in.nextLine();
+        String[] strArr = str.split(" ");
+        int len = strArr.length;
 
-        /*CQueue obj = new CQueue();
-        int param_1 = obj.deleteHead();
-        System.out.println(param_1);
-        obj.appendTail(5);
-        obj.appendTail(2);
-        int param_2 = obj.deleteHead();
-        System.out.println(param_2);
-        int param_3 = obj.deleteHead();
-        System.out.println(param_3);*/
 
-        // System.out.println(1 <= 2);
 
-        /*MinStack minStack = new MinStack();
-        minStack.push(-2);
-        minStack.push(0);
-        minStack.push(-3);
-        System.out.println(minStack.min());
-        minStack.pop();
-        System.out.println(minStack.top());
-        System.out.println(minStack.min());*/
-
-       /* Map<Character, Integer> frequency = new HashMap<Character, Integer>();
-        Integer integer = frequency.get('a');
-        System.out.println(integer);*/
-
-        /*List<Integer>[] graph = new ArrayList[100];
-        for (int i = 0; i < 100; i++) {
-            graph[i].add(i);
-        }
-
-        // List<Integer[]> graph2 = new ArrayList<>();
-        // graph2.add(new Integer[]{1,2});*/
-
-        CloseableHttpClient aDefault = HttpClients.createDefault();
-        System.out.println(aDefault);
-        CloseableHttpClient system = HttpClients.createSystem();
-        System.out.println(system);
     }
 }
---
+++ src/main/java/com/wxf/nowcoder/DecAndHexOctal.java
@@ -0,0 +1,58 @@
+package com.wxf.nowcoder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DecAndHexOctal {
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 10);
+        map.put("B", 11);
+        map.put("C", 12);
+        map.put("D", 13);
+        map.put("E", 14);
+        map.put("F", 15);
+        Scanner in = new Scanner(System.in);
+        String a = in.nextLine();
+        a = a.substring(2, a.length());
+        int b = a.length();
+        double res = 0.0;
+        int i = 0;
+        while (b > 0) {
+            String d = a.charAt(i) + "";
+            if (map.containsKey(d)) {
+                res = res + map.get(d) * Math.pow(16, (double) b - 1);
+            } else {
+                res = res + Integer.parseInt(d) * Math.pow(16, (double) b - 1);
+            }
+            i++;
+            b--;
+        }
+        System.out.println((int) res);
+
+        Map<Integer, String> map2 = new HashMap<>();
+        map2.put(10, "A");
+        map2.put(11, "B");
+        map2.put(12, "C");
+        map2.put(13, "D");
+        map2.put(14, "E");
+        map2.put(15, "F");
+        int rr = (int) res;
+        int aa = 0;
+        int bb = 0;
+        StringBuilder result = new StringBuilder();
+        while (rr % 16 > 0) {
+            aa = rr / 16;
+            bb = rr % 16;
+            if (map2.containsKey(bb)) {
+                result.append(map2.get(bb));
+            } else {
+                result.append(bb);
+            }
+            rr = aa;
+        }
+        System.out.println("0x" + result);
+    }
+}
