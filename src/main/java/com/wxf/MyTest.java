c52360b4e635dd566de2e4e9ada8edd9dc749501
wxingfu <wxffxw.664587@gmail.com>
2023-02-15 21:49:28 +0800

    20230215

--- src/main/java/com/wxf/MyTest.java
+++ src/main/java/com/wxf/MyTest.java
@@ -4,5 +4,16 @@ public class MyTest {
 
     public static void main(String[] args) {
 
+
+        int i =5;
+        int j = 10;
+        System.out.println(i);
+        System.out.println(~i);
+        System.out.println(Integer.toBinaryString(i));
+        System.out.println(Integer.toBinaryString(~i));
+        System.out.println(Integer.toBinaryString(j));
+        System.out.println(Integer.toBinaryString(~j));
+        System.out.println(~j);
+        System.out.println(i + ~j);
     }
 }
